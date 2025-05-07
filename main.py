25d1884010314b83c57ced3af84c47c47bcc310a
strongHunter <@>
2025-05-07 21:59:21 +0300

    fix: Обработка всех случаев возврата из UI

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-from ui import FuzzingMenu, FuzzingCommand
+from ui import FuzzingMenu, FuzzingCommand, UserExit
 from items_extractor import StubTargetsExtractor # TODO
 
 if __name__ == '__main__':
     extractor = StubTargetsExtractor()
 
     app = FuzzingMenu(extractor)
-    result: FuzzingCommand = app.run()
-    print(f'Command: {result.cmd}')
+    result = app.run()
+    match result:
+        case FuzzingCommand():
+            print(f'Command: {result.cmd}')
+        case UserExit():
+            exit(0)
+        case _:
+            raise TypeError(f'Unexpected return type: {type(result)}')    
